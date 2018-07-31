da7a2aeafa53fccfb435121d94b3f1c500d60a3a
Yuwei Zhou <yuwzho@microsoft.com>
2018-07-31 17:04:07 +0800

    :wrench: Add package info files

---
+++ src/main/java/com/microsoft/jenkins/acr/descriptor/package-info.java
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ */
+
+/**
+ * Sub descriptor will be used in this plugin.
+ */
+package com.microsoft.jenkins.acr.descriptor;
